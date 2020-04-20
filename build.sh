866fb2e557f93bf88ee027c5a021a260f3cd55d1
Dmytro Zagorodny <fgmmaker@gmail.com>
2020-04-20 23:44:24 +0300

    Added rmi to build script

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ docker container rm $(docker container ls -aq)
 
 docker system prune --volumes
 
+docker rmi $APP_NAME
+
 docker build -name blogger_server -t $APP_NAME .
 
 docker run -e NODE_ENV=production -p 80:5000 -d $APP_NAME 
