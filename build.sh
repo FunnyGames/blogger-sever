8cce6594b801b14dc01592a9fa9150d787074829
Dmytro Zagorodny <fgmmaker@gmail.com>
2020-04-20 23:46:23 +0300

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,10 +4,10 @@ docker container stop $(docker container ls -aq)
 
 docker container rm $(docker container ls -aq)
 
-docker system prune --volumes
+docker system prune -f --volumes
 
 docker rmi $APP_NAME
 
-docker build -name blogger_server -t $APP_NAME .
+docker build -t $APP_NAME .
 
 docker run -e NODE_ENV=production -p 80:5000 -d $APP_NAME 
